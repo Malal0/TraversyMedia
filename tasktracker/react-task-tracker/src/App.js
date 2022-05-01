3bd7efedbf3add862ee1826dec11e47a9a07ebb9
UncomfortableChai <mj1055119@gmail.com>
2022-04-30 23:29:45 -0700

    practice

--- tasktracker/react-task-tracker/src/App.js
+++ tasktracker/react-task-tracker/src/App.js
@@ -1,13 +1,12 @@
+import Header from "./components/Header";
 
 function App() {
-  const name = 'John Doe';
 
   return (
-    <div className="Container">
-      <h1>Hello From React</h1>
-      <h2>Hello {name}</h2>
+    <div className='container'>
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
