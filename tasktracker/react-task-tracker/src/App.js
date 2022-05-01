adc3757a56d1456941f74985cee8ac1ca39d44a3
UncomfortableChai <mj1055119@gmail.com>
2022-04-30 23:04:02 -0700

    started traversy media react tutorial

---
+++ tasktracker/react-task-tracker/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  const name = 'John Doe';
+
+  return (
+    <div className="Container">
+      <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
+    </div>
+  );
+}
+
+export default App;
